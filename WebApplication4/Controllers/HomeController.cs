be42fc4c7c30eab362ba34cd46a75cba11d52912
kwonwoonam <kwonwoo77@gmail.com>
2023-06-03 11:42:39 +0900

    초기 커밋.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<UserInfo> _userInfos;
+
+        public HomeController()
+        {
+            _userInfos = new List<UserInfo>()
+            {
+                new UserInfo(){ UserId = 1, UserName = "testuser01", UserPassword = "1234" },
+                new UserInfo(){ UserId = 1, UserName = "testuser02", UserPassword = "1234" },
+            };
+        }
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            // 요청한 사용자의 인증 여부를 확인한다.
+            if(User.Identity.IsAuthenticated)
+                return RedirectToAction("Profile");
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index(UserInfo userInfo)
+        {
+            string message;
+            UserInfo findUserInfo;
+
+            findUserInfo = _userInfos.Find(m => m.UserName == userInfo.UserName && m.UserPassword == userInfo.UserPassword);
+
+            if (findUserInfo == null)
+            {
+                message = "ID 또는 Password가 일치하지 않습니다.";
+            }
+            else
+            {
+                // 인증된 사용자의 폼인증 쿠키를 만든다.
+                FormsAuthentication.SetAuthCookie(userInfo.UserName, false);
+                return RedirectToAction("Profile");
+            }
+
+            ViewBag.Message = message;
+            return View(userInfo);
+        }
+
+        public ActionResult Logout()
+        {
+            // 사용자의 폼인증 쿠키를 삭제하여 로그아웃 처리한다.
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication4/Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "필수입력사항입니다.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "필수입력사항입니다.")]
+        public string UserPassword { get; set; }
+        [Required(ErrorMessage = "필수입력사항입니다.")]
+        [Compare("UserPassword", ErrorMessage = "비밀번호가 일치하지 않습니다.")]
+        public string ConfirmUserPassword { get; set; }
+    }
+}
